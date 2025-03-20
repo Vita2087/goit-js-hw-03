def94b974192b5b2def901392e3f8c8a2ba312a6
Vita20 <vitahappy20@gmail.com>
2025-03-20 22:05:40 +0200

    add tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,33 +1,15 @@
-/*
- * Задача 2. Форматування повідомлення
+// Задача 2. Композиція масивів
 
- * Оголоси функцію formatMessage(message, maxLength), 
- * яка приймає рядок (параметр message) 
- * та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
- * 
- * Доповни код функції таким чином, що:
- * Якщо довжина рядка дорівнює або менша за maxLength, 
- * то функція повертає початковий рядок без змін.
- * 
- * Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, 
- * додає трикрапку "..." в кінці та повертає обрізану версію.
- */
+// Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray (масив), secondArray (масив) і maxLength (число). Функція повинна створювати новий масив, який містить усі елементи з firstArray, а потім усі елементи з secondArray.
+// Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength елементів.
+// В іншому випадку функція повинна повернути весь новий масив.
 
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return message.slice(0, maxLength) + "...";
-  }
-}
 
-// function formatMessage(message, maxLength) {
-//   return message.lenght <= maxLength ? message : message.slice(0, maxLength) + '...';
-// }
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
-console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
